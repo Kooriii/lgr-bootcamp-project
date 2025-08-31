2b800acba15769dde938a688d259c5d6e0e8ad44
Kooriii <22679743+Kooriii@users.noreply.github>
2025-08-31 23:47:41 +0800

    Add app state

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -3,7 +3,12 @@ use std::error::Error;
 use axum::{routing::post, serve::Serve, Router};
 use tower_http::services::ServeDir;
 
+use crate::app_state::AppState;
+
+pub mod app_state;
+pub mod domain;
 pub mod routes;
+pub mod services;
 
 pub struct Application {
     server: Serve<Router, Router>,
@@ -11,14 +16,15 @@ pub struct Application {
 }
 
 impl Application {
-    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+    pub async fn build(app_state: AppState, address: &str) -> Result<Self, Box<dyn Error>> {
         let router = Router::new()
             .nest_service("/", ServeDir::new("assets"))
             .route("/signup", post(routes::signup))
             .route("/login", post(routes::login))
             .route("/verify_2fa", post(routes::verify_2fa))
             .route("/logout", post(routes::logout))
-            .route("/verify_token", post(routes::verify_token));
+            .route("/verify_token", post(routes::verify_token))
+            .with_state(app_state);
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
